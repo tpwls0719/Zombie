9b2f998f99f60de60afb4b306fb44da48ef9620a
DESKTOP-HRD488S\202_12 <ohyas_44@naver.com>
2025-07-14 14:42:34 +0900

    총 발사, 재장전

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -30,26 +30,83 @@ public class Gun : MonoBehaviour {
 
     private float lastFireTime; // 총을 마지막으로 발사한 시점
 
-    private void Awake() {
+    private void Awake()
+    {
         // 사용할 컴포넌트의 참조 가져오기
+        gunAudioPlayer = GetComponent<AudioSource>();
+        bulletLineRenderer = GetComponent<LineRenderer>();
+
+        //사용할 점을 두 개로 변경
+        bulletLineRenderer.positionCount = 2;
+        //라인 렌더러를 비활성화
+        bulletLineRenderer.enabled = false;
     }
 
-    private void OnEnable() {
+    private void OnEnable()
+    {
         // 총 상태 초기화
+        ammoRemain = gunData.startAmmoRemain;
+        magAmmo = gunData.magCapacity;
+
+        state = State.Ready;
+        lastFireTime = 0;
     }
 
     // 발사 시도
-    public void Fire() {
+    public void Fire()
+    {
+        if (state == State.Ready && Time.time >= lastFireTime + gunData.timeBetFire)
+        {
+            lastFireTime = Time.time;
+            Shot();
+        }
 
     }
 
     // 실제 발사 처리
-    private void Shot() {
+    private void Shot()
+    {
+        RaycastHit hit;
+        Vector3 hitPosition = Vector3.zero;
+
+        if (Physics.Raycast(fireTransform.position, fireTransform.forward, out hit, fireDistance))
+        {
+            IDamageable target = hit.collider.GetComponent<IDamageable>();
+
+            if (target != null)
+            {
+                target.OnDamage(gunData.damage, hit.point, hit.normal);
+            }
+
+            hitPosition = hit.point;
+        }
+        else
+        {
+            hitPosition = fireTransform.position + fireTransform.forward * fireDistance;
+        }
+
+        StartCoroutine(ShotEffect(hitPosition));
+
+        magAmmo--;
+        if (magAmmo <= 0)
+        {
+            //탄창에 남은 탄약이 없다면 총의 현재 상태를 Empty으로 갱신
+            state = State.Empty;
+        }
       
     }
 
     // 발사 이펙트와 소리를 재생하고 탄알 궤적을 그림
     private IEnumerator ShotEffect(Vector3 hitPosition) {
+        //총구 화염 효과 재생
+        muzzleFlashEffect.Play();
+        //탄피 배출 효과 재생
+        shellEjectEffect.Play();
+
+        //총격 소리 재생
+        gunAudioPlayer.PlayOneShot(gunData.shotClip);
+        bulletLineRenderer.SetPosition(0, fireTransform.position);
+        bulletLineRenderer.SetPosition(1, hitPosition);
         // 라인 렌더러를 활성화하여 탄알 궤적을 그림
         bulletLineRenderer.enabled = true;
 
@@ -61,17 +118,43 @@ public class Gun : MonoBehaviour {
     }
 
     // 재장전 시도
-    public bool Reload() {
-        return false;
+    public bool Reload()
+    {
+        if (state == State.Reloading || ammoRemain <= 0 || magAmmo >= gunData.magCapacity)
+        {
+            //이미 재장전 중이거나 남은 총알이 없거나 탄창에 총알이 이미 가득한 경우 재장전 불가
+            return false;
+        }
+
+        //재장전 처리 시작
+        StartCoroutine(ReloadRoutine());
+        return true;
     }
 
     // 실제 재장전 처리를 진행
     private IEnumerator ReloadRoutine() {
         // 현재 상태를 재장전 중 상태로 전환
         state = State.Reloading;
+
+        //재장전 소리 재생
+        gunAudioPlayer.PlayOneShot(gunData.reloadClip);
       
         // 재장전 소요 시간 만큼 처리 쉬기
         yield return new WaitForSeconds(gunData.reloadTime);
+
+        //탄창에 채울 탄약 게산
+        int ammoToFill = gunData.magCapacity - magAmmo;
+
+        //탄창에 채워야 할 탄약이 남은 탄약보다 많다면 채워야할 탄약 수를 남은 탄약 수에 맞춰 줄인다.
+        if (ammoRemain < ammoToFill)
+        {
+            ammoToFill = ammoRemain;
+        }
+
+        //탄창을 채운다
+        magAmmo += ammoToFill;
+        //남은 탄약에서 탄창에 채운 만큼 탄약을 뺀다.
+        ammoRemain -= ammoToFill;
 
         // 총의 현재 상태를 발사 준비된 상태로 변경
         state = State.Ready;
--- Assets/Scripts/PlayerShooter.cs
+++ Assets/Scripts/PlayerShooter.cs
@@ -27,21 +27,56 @@ public class PlayerShooter : MonoBehaviour {
         gun.gameObject.SetActive(false);
     }
 
-    private void Update() {
+    private void Update()
+    {
         // 입력을 감지하고 총 발사하거나 재장전
+        if (playerInput.fire)
+        {
+            gun.Fire();
+        } else if (playerInput.reload) {
+            if (gun.Reload()) {
+                playerAnimator.SetTrigger("Reload");
+            }
+        }
+        //UpdateUI();
     }
 
     // 탄약 UI 갱신
-    private void UpdateUI() {
+    /*private void UpdateUI() {
         if (gun != null && UIManager.instance != null)
         {
             // UI 매니저의 탄약 텍스트에 탄창의 탄약과 남은 전체 탄약을 표시
             UIManager.instance.UpdateAmmoText(gun.magAmmo, gun.ammoRemain);
         }
-    }
+    }*/
 
     // 애니메이터의 IK 갱신
-    private void OnAnimatorIK(int layerIndex) {
-        
+        private void OnAnimatorIK(int layerIndex) {
+        // 총의 기준점 gunPivot을 3D 모델의 오른쪽 팔꿈치 위치로 이동
+        // 이렇게 하면 총이 캐릭터의 팔꿈치 움직임에 따라 자연스럽게 위치함
+        gunPivot.position =
+            playerAnimator.GetIKHintPosition(AvatarIKHint.RightElbow);
+
+        // 왼손 IK 설정: 총의 왼쪽 손잡이에 맞춤
+        // Weight 1.0f = IK가 100% 적용됨 (애니메이션보다 IK 우선)
+        playerAnimator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1.0f);
+        playerAnimator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1.0f);
+
+        // 왼손의 실제 위치와 회전을 총의 왼쪽 손잡이로 설정
+        playerAnimator.SetIKPosition(AvatarIKGoal.LeftHand,
+            leftHandMount.position);
+        playerAnimator.SetIKRotation(AvatarIKGoal.LeftHand,
+            leftHandMount.rotation);
+
+        // 오른손 IK 설정: 총의 오른쪽 손잡이에 맞춤
+        // Weight 1.0f = IK가 100% 적용됨
+        playerAnimator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1.0f);
+        playerAnimator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1.0f);
+
+        // 오른손의 실제 위치와 회전을 총의 오른쪽 손잡이로 설정
+        playerAnimator.SetIKPosition(AvatarIKGoal.RightHand,
+            rightHandMount.position);
+        playerAnimator.SetIKRotation(AvatarIKGoal.RightHand,
+            rightHandMount.rotation);
     }
 }
